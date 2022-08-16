489b9e50fc56f9266ff780f6d85bd64f0f7c18a2
Filin-oleg <110037084+Filin-oleg@users.noreply.github.com>
2022-08-16 18:42:49 +0200

    done

---
+++ Task001/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.WriteLine("Введите первое число");
+int numA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int numB = int.Parse(Console.ReadLine());
+
+double Method(int numberA, int numberB)
+{
+    double numC = Math.Pow(numberA, numberB);
+    return numC;
+}
+double numD = Method(numA, numB);
+Console.WriteLine("Первое число возведенное в степень второго числа = " + numD);

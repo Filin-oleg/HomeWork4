f581f364028b2fa031effce7b50fa5f5fbec121f
Filin-oleg <110037084+Filin-oleg@users.noreply.github.com>
2022-08-18 16:28:32 +0200

    done

--- Task003/Program.cs
+++ Task003/Program.cs
@@ -1,14 +1,13 @@
 ﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 
-
-void randomArrayfill (int arrLenght)
+void randomArrayfill (int arrLenght, int min, int max)
 {
     int[] array = new int[arrLenght];
     for (int i = 0; i < arrLenght; i++)
     {
-        array[i] = new Random().Next(1, 10);
+        array[i] = new Random().Next(min, max);
         Console.Write(array[i] + " ");
     }
 }
-randomArrayfill (8);
+randomArrayfill (8, 1, 10);
 Console.WriteLine("");
